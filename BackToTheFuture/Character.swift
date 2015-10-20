f97d45b14ce946b5a4b4715b1428bb3663cf8763
akozlik <akozlik@gmail.com>
2015-10-20 00:03:49 -0400

    App is crashing but time for bed

---
+++ BackToTheFuture/Character.swift
@@ -0,0 +1,30 @@
+//
+//  Character.swift
+//  BackToTheFuture
+//
+//  Created by Andrew Kozlik on 10/19/15.
+//  Copyright © 2015 Andrew Kozlik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Character: NSObject {
+    var characterName : String?
+    var actorName : String?
+    var actorHeadshot : UIImage?
+    var actorHeadshotName : String?
+    
+    init(characterName : String, actorName : String, actorHeadshotName : String?) {
+        super.init();
+        
+        self.characterName = characterName
+        self.actorName = actorName
+        
+        if (actorHeadshot != nil) {
+            self.actorHeadshot = UIImage(named: actorHeadshotName!)
+            self.actorHeadshotName = actorHeadshotName
+        }
+
+    }
+}
--- BackToTheFuture/ViewController.swift
+++ BackToTheFuture/ViewController.swift
@@ -8,12 +8,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var buttonTimeTravel : UIButton?
+    @IBOutlet weak var tableView : UITableView?
+    
+    let message2015 = "Time Travel to 2015"
+    let message1985 = "Time Travel to 1985"
+    
+    var characters : NSMutableArray?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (characters == nil) {
+            characters = NSMutableArray();
+        }
+        
+        let marty = Character.init(characterName: "Marty McFly", actorName: "Michael J Fox", actorHeadshotName: "michael")
+        let doc = Character.init(characterName: "Doc Brown", actorName: "Christopher Lloyd", actorHeadshotName: "christopher")
+        let lorraine = Character.init(characterName: "Lorraine Baines", actorName: "Lea Thompson", actorHeadshotName: "lea")
+        
+        characters?.addObjectsFromArray([marty, doc, lorraine])
+        
+        var shortcutItems = [UIMutableApplicationShortcutItem]()
+        
+        for character in characters! {
+            let c = character as! Character
+            let item = UIMutableApplicationShortcutItem(
+                type: "com.codefortravel.backtothefuture.character",
+                localizedTitle: c.characterName!,
+                localizedSubtitle: "Tap for more details",
+                icon: UIApplicationShortcutIcon(templateImageName: c.actorHeadshotName!),
+                userInfo: nil
+            )
+            shortcutItems.append(item)
+        }
+        
+        UIApplication.sharedApplication().shortcutItems = shortcutItems
+
+        NSLog("Characters: %@", characters!);
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: IBAction Methods
+    
+    @IBAction func timeTravel(sender:UIButton) {
+        if (sender.titleLabel!.text == message2015) {
+            sender.setTitle(message1985, forState: UIControlState.Normal)
+        } else if (sender.titleLabel!.text == message1985) {
+            sender.setTitle(message2015, forState: UIControlState.Normal)
+        }
+    }
+    
+    // MARK: UITableViewDelegate/DataSource Methods
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier")
+        
+        if (cell != nil) {
+            let character : Character = (characters?.objectAtIndex(indexPath.row))! as! Character
+            
+            cell?.textLabel?.text = character.characterName
+            cell?.detailTextLabel?.text = character.actorName
+            cell?.imageView?.image = character.actorHeadshot
+        }
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters!.count
+    }
+    
+    // MARK: Memory Warning Method
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
